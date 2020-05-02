1be4ba66e94b41206df5e9ab2c83e4edbee992d2
kangtaeheon <t01020559053@gmail.com>
2020-05-02 21:06:10 +0900

    project MM 0502_commit kang_(문제점 : Node.js 없이 진행할 수 있을까?)

---
+++ js/movies.js
@@ -0,0 +1,5 @@
+var searchBtn = document.getElementById("search_btn");
+searchBtn.onclick = function(){
+    var keyword = document.getElementById("searchKeyword").value;
+    console.log(keyword);
+}
