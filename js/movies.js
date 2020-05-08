d40c25676b9d0eff76e8e0f0b33c8f7e84acd05e
kangtaeheon <t01020559053@gmail.com>
2020-05-08 14:03:48 +0900

    commit;

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,27 @@
+// var searchBtn = document.getElementById("search_btn");
+// searchBtn.onclick = function(){
+//     var keyword = document.getElementById("searchKeyword").value;
+//     console.log(keyword);
+// }
+
+// $("search_btn").click({
+//     function(){
+
+//     }
+// })
+
+// document.getElementById("search_btn").onclick({
+//     function(){
+//         alert("HEllo world!")
+//     }
+// })
+
 var searchBtn = document.getElementById("search_btn");
-searchBtn.onclick = function(){
-    var keyword = document.getElementById("searchKeyword").value;
-    console.log(keyword);
-}
+
+// searchBtn.addEventListener("click", () => {
+//     console.log("난간지남이다");
+// })
+
+searchBtn.addEventListener("click", function() {
+    console.log("난간지남이다");
+})
