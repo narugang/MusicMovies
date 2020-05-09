748595f9c5ff43bb2b2548965c5f4c099beecc0e
kangtaeheon <t01020559053@gmail.com>
2020-05-09 17:38:31 +0900

    0509 commit NO01 movies and home and requestPage commit;

--- js/movies.js
+++ js/movies.js
@@ -1,27 +1,9 @@
-// var searchBtn = document.getElementById("search_btn");
-// searchBtn.onclick = function(){
-//     var keyword = document.getElementById("searchKeyword").value;
-//     console.log(keyword);
-// }
-
-// $("search_btn").click({
-//     function(){
-
-//     }
-// })
-
-// document.getElementById("search_btn").onclick({
-//     function(){
-//         alert("HEllo world!")
-//     }
-// })
 
 var searchBtn = document.getElementById("search_btn");
 
-// searchBtn.addEventListener("click", () => {
-//     console.log("난간지남이다");
-// })
-
 searchBtn.addEventListener("click", function() {
-    console.log("난간지남이다");
+    var keyword = document.getElementById("searchKeyword").value;
+    alert(keyword+" "+"[ 준비중 입니다 ]")
+
+
 })
