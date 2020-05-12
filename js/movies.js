6e61fbde9404fd89f78e795e81f0e54b2837a756
kangtaeheon <t01020559053@gmail.com>
2020-05-12 19:51:58 +0900

    0512 02 commit;

--- js/movies.js
+++ js/movies.js
@@ -1,9 +1,6 @@
-
 var searchBtn = document.getElementById("search_btn");
 
-searchBtn.addEventListener("click", function() {
-    var keyword = document.getElementById("searchKeyword").value;
-    alert(keyword+" "+"[ 준비중 입니다 ]")
-
-
-})
+searchBtn.addEventListener("click", function () {
+  var keyword = document.getElementById("searchKeyword").value;
+  alert(keyword + " " + "[ 준비중 입니다 ]");
+});
