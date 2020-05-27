987ac5393058a6a22bc073cd87fd25186574921a
kangtaeheon <t01020559053@gmail.com>
2020-05-27 16:56:31 +0900

    0527 01 movies kang commit;

--- js/movies.js
+++ js/movies.js
@@ -1,7 +1,16 @@
 //검색 버튼[S]
 var searchBtn = document.getElementById("search_btn");
 
-searchBtn.addEventListener("click", function () {
+function enterkey() {
+  if (window.event.keyCode == 13) {
+    // 엔터키가 눌렸을 때 실행할 내용
+    searchMovies();
+  }
+}
+
+searchBtn.onclick = searchMovies;
+
+function searchMovies() {
   var keyword = document.getElementById("searchKeyword").value;
 
   var requestURL = "https://yts.mx/api/v2/list_movies.json?sort_by=rating";
@@ -142,7 +151,157 @@ searchBtn.addEventListener("click", function () {
       underElement.appendChild(summury);
     }
   }
-});
+}
+//여기까지 내용 지우기
+
+// searchBtn.onclick = dd;
+
+// function dd() {
+//   alert("hello");
+// }
+
+// searchBtn.addEventListener("click", function searchMovies() {
+//   var keyword = document.getElementById("searchKeyword").value;
+
+//   var requestURL = "https://yts.mx/api/v2/list_movies.json?sort_by=rating";
+//   var request = new XMLHttpRequest();
+//   request.open("GET", requestURL);
+//   console.log(request);
+//   request.responseType = "json";
+//   request.send();
+//   console.log(request);
+
+//   //응답이 반환 된 후
+//   request.onload = function () {
+//     //응답을 변수에 담는다. (movies)
+//     var movies = request.response;
+//     console.log(movies);
+//     //변수에 담은(movies)를 JS 객체에 포함한다.
+//     showMovies(movies);
+//   };
+
+//   function showMovies(userSearch) {
+//     var userSearchContext = new Array();
+
+//     for (var i in userSearch.data.movies) {
+//       //요렇게 비교해서 검색하면 될 듯
+//       var userKey = keyword.toUpperCase();
+//       var jsonTitle = userSearch.data.movies[i].title.toUpperCase();
+
+//       console.log("사용자 검색어 : " + userKey);
+//       console.log("Jsonfile내용 : " + jsonTitle);
+//       console.log(
+//         "단어 비교 (-1) 아니면 중복이 있음 : " + jsonTitle.indexOf(userKey)
+//       );
+
+//       if (jsonTitle.indexOf(userKey) != -1) {
+//         userSearchContext.push(userSearch.data.movies[i]);
+//       }
+//       for (var j in userSearch.data.movies[i].genres) {
+//         var jsonGenres = userSearch.data.movies[i].genres[j].toUpperCase();
+//         console.log(jsonGenres.indexOf(userKey));
+//         if (jsonGenres.indexOf(userKey) != -1) {
+//           userSearchContext.push(userSearch.data.movies[i]);
+//         }
+//       }
+//       if (document.getElementById("testSec" + i) != null) {
+//         var oldDiv = document.getElementById("testSec" + i);
+//         oldDiv.remove();
+//       }
+//     }
+//     console.log(userSearchContext.length);
+//     for (i = 0; i < userSearchContext.length; i++) {
+//       var element = document.createElement("div");
+//       element.id = "testSec" + i;
+
+//       var addChild = document.getElementById("movieContent");
+//       addChild.appendChild(element);
+
+//       //Element 아래 하위 요소들
+//       var title = document.createElement("div");
+//       title.id = "title" + i;
+//       title.textContent = userSearchContext[i].title_long;
+
+//       var rating = document.createElement("div");
+//       rating.id = "rating" + i;
+//       rating.textContent = "평점 : " + userSearchContext[i].rating;
+
+//       var runtime = document.createElement("div");
+//       runtime.id = "runtime" + i;
+//       runtime.textContent = "상영 시간 : " + userSearchContext[i].runtime;
+
+//       var years = document.createElement("div");
+//       years.id = "year" + i;
+//       years.textContent = "year : " + userSearchContext[i].year;
+
+//       // genres[s]
+//       var genres = document.createElement("div");
+//       genres.id = "genres" + i;
+//       genres.textContent = "genres : " + userSearchContext[i].genres;
+//       // genres[e]
+
+//       var img = document.createElement("img");
+//       img.id = "img" + i;
+//       img.src = userSearchContext[i].medium_cover_image;
+
+//       var summury = document.createElement("div");
+//       summury.id = "summ" + i;
+//       summury.style.width = "500px";
+//       summury.style.height = "550px";
+//       summury.style.backgroundColor = "white";
+//       summury.style.display = "none";
+//       summury.textContent = userSearchContext[i].description_full;
+
+//       var summuryBtn = document.createElement("button");
+//       summuryBtn.value = i;
+//       summuryBtn.onclick = function () {
+//         //windo.open() 을 이용해 새로운 창을 열고 그 곳에서 요약 내용을 보여주는 방법.
+//         if (
+//           (document.getElementById("summ" + this.value).style.display = "none")
+//         ) {
+//           var win = window.open(
+//             "",
+//             "_blank",
+//             "width=600px, height=300px, left=50%, top=50%"
+//           );
+//           var titleDiv = win.document.createElement("div");
+//           titleDiv.textContent = "summary";
+//           titleDiv.style.fontSize = "50px";
+
+//           // 카테고리를 보여주고 싶어(0516 시작할 것)
+//           var summaryText = win.document.createElement("div");
+//           summaryText.textContent = document.getElementById(
+//             "summ" + this.value
+//           ).textContent;
+
+//           var winBtn = win.document.createElement("button");
+//           winBtn.textContent = "close";
+//           winBtn.onclick = function () {
+//             win.close();
+//           };
+//           win.document.body.appendChild(titleDiv);
+//           win.document.body.appendChild(summaryText);
+//           win.document.body.appendChild(winBtn);
+//         }
+//       };
+
+//       summuryBtn.textContent = "줄거리 보기";
+//       summuryBtn.id = "sumBtn" + i;
+//       summuryBtn.className = "sumBtn";
+
+//       // 만들어진 요소들을 movieContent\testSec\ 으로 append
+//       var underElement = document.getElementById("testSec" + i);
+//       underElement.appendChild(img);
+//       underElement.appendChild(title);
+//       underElement.appendChild(years);
+//       underElement.appendChild(rating);
+//       underElement.appendChild(runtime);
+//       underElement.appendChild(genres);
+//       underElement.appendChild(summuryBtn);
+//       underElement.appendChild(summury);
+//     }
+//   }
+// });
 //검색 버튼[E]
 
 var requestURL = "https://yts.mx/api/v2/list_movies.json?sort_by=rating";
